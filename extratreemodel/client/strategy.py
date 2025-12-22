49a2a395bc0fe4735cbc93a72aaff5d770e21da0
giovanniscirocco <giovannisciro03@gmail.com>
2025-12-22 17:07:44 +0100

    ho risolto NAN

---
+++ extratreemodel/client/strategy.py
@@ -0,0 +1,3 @@
+from extratreemodel.server.strategy import ExtraTreesEnsemble
+
+
--- extratreemodel/server/config.py
+++ extratreemodel/server/config.py
@@ -5,7 +5,7 @@ SERVER_ADDRESS = "127.0.0.1:8080"
 # Numero di round Flower:
 # round 1 = feature selection
 # round 2..NUM_ROUNDS = training + build ensemble
-NUM_ROUNDS = 10
+NUM_ROUNDS = 2
 
 # Client tenuto fuori dal training e usato come holdout finale (0..8). Metti 9 o 10 per disattivare.
 HOLDOUT_CID = 2
