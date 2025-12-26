c0059ed8cf20e39bcd253385b95ae663fffc702b
AlessandroTinaoui <tinaouialessandro02@gmail.com>
2025-12-27 00:35:41 +0100

    fix

--- extratreemodel/server/config.py
+++ extratreemodel/server/config.py
@@ -8,15 +8,14 @@ SERVER_ADDRESS = "127.0.0.1:8080"
 if "FL_SERVER_ADDRESS" in os.environ:
     SERVER_ADDRESS = os.environ["FL_SERVER_ADDRESS"]
 
-HOLDOUT_CID = int(os.environ.get("HOLDOUT_CID", 0))
+HOLDOUT_CID = int(os.environ.get("HOLDOUT_CID", 10))
 
 # Numero di round Flower:
 # round 1 = feature selection
 # round 2..NUM_ROUNDS = training + build ensemble
-NUM_ROUNDS = 10
+NUM_ROUNDS = 2
 
 # Client tenuto fuori dal training e usato come holdout finale (0..8). Metti 9 o 10 per disattivare.
-HOLDOUT_CID = 2
 # Feature selection
 TOP_K_FEATURES = 32
 
